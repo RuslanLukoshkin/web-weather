09f56d5d29b25b3ab1bb5fffd58f1f9162696e9f
RuslanLukoshkin <lukoshkin.rus@yandex.ru>
2024-03-30 11:09:44 +0300

    Добавлен файл конфига, с ссылками для подключения и ключем API

---
+++ lib/config.dart
@@ -0,0 +1,3 @@
+const apiKey = "75302080a79a7ae05dd6510d1bd1a841";
+const forecastURL = "https://api.openweathermap.org/data/2.5/";
+const reverseGeoURL = "https://api.openweathermap.org/geo/1.0/reverse";
