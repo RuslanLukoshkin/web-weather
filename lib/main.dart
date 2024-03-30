b6c61204a05480fb93368da46dfbec44c54d45a1
RuslanLukoshkin <lukoshkin.rus@yandex.ru>
2024-03-30 08:40:44 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData =
+        ThemeData(useMaterial3: true, primaryColor: Colors.black);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: themeData,
+    );
+  }
+}
